452407864ca0de777da3de315c4804a39fac6936
kang7024 <106382644+kang7024@users.noreply.github.com>
2025-11-10 00:41:47 +0900

    Update __init__.py

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -5,9 +5,11 @@ from .non_shared_controller import NonSharedMAC
 from .maddpg_controller import MADDPGMAC
 from .hygma_controller import HYGMA
 from .gmm_hygma_controller import GMMHYGMA
+from .aerial_hygma_controller import HYGMA as AerialHYGMA  # 🔹 추가
 
 REGISTRY["basic_mac"] = BasicMAC
 REGISTRY["non_shared_mac"] = NonSharedMAC
 REGISTRY["maddpg_mac"] = MADDPGMAC
 REGISTRY["hygma_mac"] = HYGMA
 REGISTRY["gmm_hygma_mac"] = GMMHYGMA
+REGISTRY["aerial_hygma_mac"] = AerialHYGMA  # 🔹 추가
